4eef20ea89166a4345a47d4896f4971a9d2897fb
Penghao-99 <79646035+Penghao-99@users.noreply.github.com>
2022-03-18 14:37:50 +0800

    Update urls.py

--- AppStore/urls.py
+++ AppStore/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('view/<str:id>', app.views.view, name='view'),
     path('edit/<str:id>', app.views.edit, name='edit'),
     path('Listings', app.views.Listings, name='Listings'),
-    path('Listings/view/<str:id>', app.views.view_Listings, name='view_Listings'),
+    path('Listings/view/<str:id>', app.views.view_Listings, name='view_Listings'), #the html files button href will refer to path(this,X,X)
     path('Unavailable', app.views.Unavailable, name='Unavailable'),
     path('Rentals', app.views.Rentals, name='Rentals'),
 ]
