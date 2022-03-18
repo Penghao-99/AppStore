9cd7e554a0b9f44c9ccf5bb51ab19719fedfb329
Penghao-99 <79646035+Penghao-99@users.noreply.github.com>
2022-03-18 11:42:39 +0800

    Update urls.py

--- AppStore/urls.py
+++ AppStore/urls.py
@@ -21,11 +21,11 @@ import app.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', app.views.index, name='index'),
+    path('', app.views.Customers, name='Customers'),
     path('add', app.views.add, name='add'),
     path('view/<str:id>', app.views.view, name='view'),
     path('edit/<str:id>', app.views.edit, name='edit'),
-    path('Customers', app.views.Customers, name='Customers'),
+
     path('Listings', app.views.Listings, name='Listings'),
     path('Unavailable', app.views.Unavailable, name='Unavailable'),
     path('Rentals', app.views.Rentals, name='Rentals'),
